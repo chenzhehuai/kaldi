08f8dff529e2dfdabd3928f5ee89bb539283eb0b
chenzhehuai <chenzhehuai@foxmail.com>
2018-02-03 17:39:13 -0500

    some defi & init

---
+++ src/fstbin/afstcombine.cc
@@ -0,0 +1,65 @@
+// See www.openfst.org for extensive documentation on this weighted
+// finite-state transducer library.
+//
+// Performs the dynamic replacement of arcs in one FST with another FST,
+// allowing for the definition of FSTs analogous to RTNs.
+
+#include <cstring>
+
+#include <string>
+#include <vector>
+#include <fst/flags.h>
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "fstext/kaldi-fst-io.h"
+
+using LabelFstPair = std::pair<int, const fst::Fst<fst::StdArc> *>;
+
+int main(int argc, char **argv) {
+  try {
+    using namespace kaldi;
+    using namespace fst;
+    
+    bool del_disambig_sym = true;
+    string disambig_wxfilename;
+    const char *usage = "Recursively replaces FST arcs with other FST(s).\n\n"
+                   "  Usage: "
+                   "afstcombine root.fst labelmap [rule1.fst label1 labelmap  ...] [out.fst]\n";
+
+    ParseOptions po(usage);
+    AfstCombineOptions opts;
+    po.Register("connect", &opts.connect, "If true, trim FST before output.");
+    po.Register("del-disambig-sym", &opts.del_disambig_sym, "If true, delete 
+                disambig symbols after concatenation.");
+    po.Register("write-disambig-syms", &disambig_wxfilename,
+                "List of disambiguation symbols on input of out.fst");
+    po.Read(argc, argv);
+    if (po.NumArgs() < 6) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    AFSTCombine<StdArc> afst_combine_data(opts);
+
+    const string fst_name = po.GetArg(1);
+    const string disam_map_name = po.GetArg(2);
+    const string fst_out_str =  po.GetArg(po.NumArgs());
+    if (afst_combine_data.InitHfst(fst_name, disam_map_name)) return 1;
+    for (auto i = 3; i < po.NumArgs(); i += 3) {
+      const string fst_name = po.GetArg(i);
+      const auto label = atoll(po.GetArg(i+1).c_str());
+      const string disam_map_name = po.GetArg(i+2);
+      if (afst_combine_data.InitSingleAfst(fst_name, label, disam_map_name)) return 1;
+    }
+    if (afst_combine_data.CombineMain()) return 1;
+    afst_combine_data.WriteCombineResult(fst_out_str);
+
+    if (disambig_wxfilename != "") {
+      if (afst_combine_data.WriteDisamSym(disambig_wxfilename)) return 1;
+    }
+    return 0;
+  } catch(const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
---
+++ src/fstext/combine-afst.h
@@ -0,0 +1,241 @@
+// See www.openfst.org for extensive documentation on this weighted
+// finite-state transducer library.
+//
+// Functions and classes to compute the concatenation of two FSTs.
+
+#ifndef AFST_CONCAT_H_
+#define AFST_CONCAT_H_
+
+#include <algorithm>
+#include <vector>
+
+#include <fst/mutable-fst.h>
+
+
+namespace fst {
+
+
+struct AfstCombineOptions {
+  bool connect;  // Connect output
+  bool del_disambig_sym;  // after combination 
+
+  AfstConcatOptions() : connect(true), del_disambig_sym(true) {}
+};
+
+template <class Arc>
+class AFSTCombine {
+public:
+  using namespace kaldi;
+  using Label = typename Arc::Label;
+  using Weight = typename Arc::Weight;
+  using StateId = typename Arc::StateId;
+  using SoaEoaPair = std::pair<StateId, StateId>;
+  using SoaEoaPairVec = std::vector<SoaEoaPair>
+  using DisamSymMap = unordered_map<Label, Label>;
+
+
+  AFSTCombine(AfstCombineOptions opts) : opts_(opts), afst_num_(0), 
+              disambig_sym_start_(std::INT_MAX), disambig_sym_end_(0) {}
+
+  CombineAfst() {
+    // TODO:
+
+  }
+  ~CombineAfst() {
+    if (hfst_) delete(hfst_);
+    CleanupFstVec(afst_vec_);
+  }
+
+  int CombineMain() {
+
+
+    if (opts_.del_disambig_sym || opts_.connect) {
+      assert(0);
+    }
+  }
+
+  const Fst* GetHfst() const { return hfst_; }
+
+  const void WriteCombineResult(const std::string& fst_name) const { 
+    WriteFstKaldi(*hfst_, fst_name);
+  }
+
+  int InitHfst(const std::string& fst_name, const std::string& map_name) {
+    LoadFstWithSymMap(fst_name, map_name, hfst_, hfst_disam_map_);
+  }
+  
+  int InitSingleAfst(const std::string& fst_name, const Label& afst_id, 
+    const std::string& map_name) {
+    afstid2vecid_map_[afst_id] = afst_num_;
+    afst_disam_map_vec_.emplace_back();
+    afst_vec_.emplace_back();
+    LoadFstWithSymMap(fst_name, map_name, afst_vec_[afst_num_], 
+                      afst_disam_map_vec_[afst_num_]);
+    afst_num_++;
+  }
+
+  // NOTICE: actually, this information can be saved in previous processing
+  void InitAfstSoaEoaPair(Fst& fst, DisamSymMap& map, SoaEoaPairVec& info_pair_vec) {
+    info_pair_vec.resize(disambig_sym_end_ - disambig_sym_start_ + 1);
+    const auto numstates1 = fst.NumStates();
+    for (StateId s1 = 0; s1 < numstates1; ++s1) {
+      for (ArcIterator<Fst<Arc>> aiter(fst, s1); !aiter.Done(); aiter.Next()) {
+        auto arc = aiter.Value();
+        if (map.count(arc.ilabel) > 0) {//it's disambig
+          //get which pair from map[arc.ilabel];
+          //according to SOA or EOA, modify the pair.first or pair.second
+          Label disam_id = map[arc.ilabel];
+          SoaEoaPair& pair = info_pair_vec[disam_id - disambig_sym_start_];
+          if it is SOA {
+            pair.first = arc.nextstate;
+          } else {
+            pair.second = s1;
+          }
+        }
+      }
+    }
+  }
+
+
+private:
+  int LoadFstWithSymMap(const std::string& fst_name, const std::string& map_name,
+                        Fst*& fst, DisamSymMap& map) {
+    fst = ReadFstKaldi(fst_name);
+    if (fst) {
+      return 0;
+    } else {
+      KALDI_ERR << "read FST error in: " << fst_name;
+      return 1;
+    }
+    std::vector<std::vector<Label>> vec;
+    if (!ReadIntegerVectorVectorSimple(map_name, &vec))
+      KALDI_ERR << "Error reading label map from " << map_name;
+    return 1;
+    for (const auto &n : vec) {
+      assert(n.size() == 2);
+      map[n[0]] = n[1];
+      //actually, this information can be saved in previous processing
+      disambig_sym_start_ = std::min(disambig_sym_start_, n[1]);
+      disambig_sym_end_ = std::max(disambig_sym_end_, n[1]);
+    }
+  }
+  void CleanupFstVec(std::vector<Fst*> fst_vec) {
+    for (const auto &n : fst_vec) if (n) delete(n);
+    fst_vec->clear();
+  }
+
+  std::unordered_map<int32, int32> afstid2vecid_map_;
+  std::vector<DisamSymMap> afst_disam_map_vec_;
+  std::vector<SoaEoaPairVec> afst_soa_eoa_pair_vec_vec_;
+  std::vector<Fst*> afst_vec_;
+  int32 afst_num_;
+  int32 disambig_sym_start_;
+  int32 disambig_sym_end_;
+
+  SoaEoaPairVec hfst_soa_eoa_pair_vec_;
+  DisamSymMap hfst_disam_map_;
+  MutableFst<Arc>* hfst_;
+  AfstCombineOptions opts_;
+}
+
+template <class Arc>
+void ConcatAfst(MutableFst<Arc> *fst1, const Fst<Arc> &fst2, 
+  const AfstConcatOptions &opts = AfstConcatOptions()) {
+  using Label = typename Arc::Label;
+  using StateId = typename Arc::StateId;
+  using Weight = typename Arc::Weight;
+  using SoaPair = pair<StateId, Weight>;
+  // Checks that the symbol table are compatible.
+  if (!CompatSymbols(fst1->InputSymbols(), fst2.InputSymbols()) ||
+      !CompatSymbols(fst1->OutputSymbols(), fst2.OutputSymbols())) {
+    FSTERROR() << "Concat: Input/output symbol tables of 1st argument "
+               << "does not match input/output symbol tables of 2nd argument";
+    fst1->SetProperties(kError, kError);
+    return;
+  }
+  const auto props1 = fst1->Properties(kFstProperties, false);
+  const auto props2 = fst2.Properties(kFstProperties, false);
+  const auto start1 = fst1->Start();
+  const auto start2 = fst2.Start();
+  if (start1 == kNoStateId) {
+    if (props2 & kError) fst1->SetProperties(kError, kError);
+    return;
+  }
+  const auto numstates1 = fst1->NumStates();
+  if (fst2.Properties(kExpanded, false)) {
+    fst1->ReserveStates(numstates1 + CountStates(fst2));
+  }
+  unordered_map<int32, SoaPair> soa_of_new_fst2;
+  SoaPair default_pair;
+  default_pair.first = kNoStateId;
+  default_pair.second = 0;
+  for (auto val : opts.disambig_in)
+  {
+    soa_of_new_fst2[val]=default_pair; //default state
+  }
+  if (start2 != kNoStateId) {
+    fst1->SetProperties(ConcatProperties(props1, props2), kFstProperties);
+  } else {
+    KALDI_ERR << "fst2 has no Start()";
+  }
+  //get whole fst2
+  for (StateIterator<Fst<Arc>> siter2(fst2); !siter2.Done(); siter2.Next()) {
+    const auto s1 = fst1->AddState();
+    const auto s2 = siter2.Value();
+    fst1->SetFinal(s1, fst2.Final(s2));
+    fst1->ReserveArcs(s1, fst2.NumArcs(s2));
+    for (ArcIterator<Fst<Arc>> aiter(fst2, s2); !aiter.Done(); aiter.Next()) {
+      auto arc = aiter.Value();
+      arc.nextstate += numstates1;
+      if (s2 == start2 && soa_of_new_fst2.count(arc.ilabel) > 0) {
+        soa_of_new_fst2[arc.ilabel].first = arc.nextstate;
+        soa_of_new_fst2[arc.ilabel].second = arc.weight;
+      }
+      fst1->AddArc(s1, arc);
+    }
+  }
+  //check #SOA in fst2
+  for (auto val : opts.disambig_in)
+  {
+    if (soa_of_new_fst2[val].first == kNoStateId) {
+      KALDI_LOG << "symbol " << val << " not found in the start of fst2;"
+      << "this symbol should be normal disambig symbol from LG.fst," <<
+      " thus we won't concat it from fst1 to fst2";
+      soa_of_new_fst2.erase(val);
+    }
+  }  
+  
+  //connect fst1 & fst2
+  for (StateId s1 = 0; s1 < numstates1; ++s1) {
+    //disconnect previous fst1 final state
+    //fst1->SetFinal(s1, Weight::Zero());
+    for (ArcIterator<Fst<Arc>> aiter(*fst1, s1); !aiter.Done(); aiter.Next()) {
+      auto arc = aiter.Value();
+      if (s1 != start1 && soa_of_new_fst2.count(arc.ilabel) > 0
+        && arc.nextstate != soa_of_new_fst2[arc.ilabel].first) {
+        Weight final_weight = fst1->Final(arc.nextstate);
+        if (final_weight == Weight::Zero()) {
+            final_weight = 0;
+            KALDI_WARN << "call eps removal before this function ";
+        }
+        arc.nextstate = soa_of_new_fst2[arc.ilabel].first;
+        arc.weight = arc.weight.Value()+soa_of_new_fst2[arc.ilabel].second.Value()+final_weight.Value();
+        if (opts.del_disambig_sym)
+        {
+            arc.ilabel = 0; //after concatenation, delete disambig_syms_ 
+        }
+        fst1->AddArc(s1, arc);
+      }
+    }
+  }
+  for (StateId s1 = 0; s1 < numstates1; ++s1) {
+      fst1->SetFinal(s1, Weight::Zero());
+  }
+
+  if (opts.connect) Connect(fst1);
+}
+
+
+}  // namespace fst
+
+#endif  // FST_CONCAT_H_
