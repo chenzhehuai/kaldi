66fcb07f677423f39415f5314e7bfa4ce3319a16
Zhehuai Chen <zchen@hsw216.cm.cluster>
2018-03-01 07:55:25 -0800

    imp a fast copy test

--- src/decoder/cuda-decoder_test2.cu
+++ src/decoder/cuda-decoder_test2.cu
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <stdio.h>
-#include <cuda.h>
 #include "base/timer.h"
-
+#include <cuda.h>
+#include "cuda_device_runtime_api.h"
+#include <curand.h>
+#include <curand_kernel.h>
 #ifdef __CUDACC__
   #define HOST __host__
   #define DEVICE __device__
@@ -69,6 +71,37 @@ DEVICE void release_semaphore(volatile int *lock){
   myadd<100,1>(ret, mutex);
   //myadd<32,2>(ret, mutex);
   }
+  __global__ void memcp(int *to, int **from, int len, int size, cudaStream_t st) {
+    //for (int i=0;i<size;i++) 
+    int i=blockIdx.x;
+    cudaMemcpyAsync(to+i*len/sizeof(int), from[i], len, cudaMemcpyDeviceToDevice);
+    //}
+  }
+  __global__ void memcp2(int *to, int **from, int len, int size, cudaStream_t st) {
+    //for (int i=0;i<size;i++) 
+    int i=blockIdx.x;
+    int tid = threadIdx.x;
+    int sz = len/sizeof(int);
+    for(; tid < sz; tid += blockDim.x) {
+      to[tid+i*blockDim.x]=from[i][tid];
+    }
+    //cudaMemcpyAsync(to+i*len/sizeof(int), from[i], len, cudaMemcpyDeviceToDevice);
+    //}
+  }
+
+
+__global__ void random(int** result, int m, int n) {
+  curandState_t state;
+  curand_init(0, /* the seed controls the sequence of random values that are produced */
+                    0, /* the sequence number is only important with multiple cores */
+                                  0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
+                                                &state);
+  if (threadIdx.x==0){
+    for (int j=0;j<m;j++)
+    for (int i=0;i<n;i++)
+  result[j][i] = curand(&state)%10;
+  }
+}
 int main() {
   //int blocks=200;
   int blocks=3;
@@ -94,24 +127,41 @@ int main() {
 */
 
   int test=100;
-  int *v_man, *vv, *v_man_f[100], *v_man_h;
+  int *v_man, *vv, *v_man_f[100], *v_man_h, s, **v_man_d;
   for (int i=0; i<test;i++) {
     cudaMalloc((void**)&v_man_f[i],sizeof(int)*n);  
   }
   std::cout<<sizeof(int)*n*test<<std::endl;
   cudaMallocHost((void**)&v_man_h,sizeof(int)*n*test);  
   cudaMalloc((void**)&v_man,sizeof(int)*n*test);  
+  cudaMalloc((void**)&v_man_d,sizeof(int*)*test);  
+  cudaMemcpy(v_man_d, v_man_f, sizeof(int*)*test, cudaMemcpyHostToDevice);
   cudaCheckError();
   cudaStream_t stream_comp;
+  cudaStream_t stream_comp2;
   cudaStreamCreateWithFlags(&stream_comp, cudaStreamNonBlocking);
+  cudaStreamCreateWithFlags(&stream_comp2, cudaStreamNonBlocking);
+  //for (int i=0; i<test;i++)
+  random<<<1,1,0,stream_comp>>>(v_man_d, test, n);
+  cudaStreamSynchronize(stream_comp);
+  cudaCheckError();
 
   double t1,t2_1,t2_2;
+  cudaMemset(v_man_h, 0,sizeof(int)*test*n);
   timer.Reset();
   for (int i=0; i<test;i++)
     cudaMemcpyAsync(v_man_h+i*n,v_man_f[i],sizeof(int)*n,cudaMemcpyDeviceToHost, stream_comp);
   cudaCheckError();
   cudaStreamSynchronize(stream_comp);
   t1=timer.Elapsed();
+
+  s=0;
+  for (int i=0; i<test; i++) for (int j=0; j<n; j++) s+=v_man_h[i*n+j];
+  int s1=s;
+
+
+  cudaMemset(v_man_h, 0,sizeof(int)*test*n);
+  cudaMemset(v_man, 0,sizeof(int)*test*n);
 
   timer.Reset();
   for (int i=0; i<test;i++)
@@ -123,7 +173,49 @@ int main() {
   cudaStreamSynchronize(stream_comp);
   t2_2=timer.Elapsed();
 
-  std::cout<<t1<<" "<<t2_1<< " "<<t2_2<<std::endl;
+  s=0;
+  for (int i=0; i<test; i++) for (int j=0; j<n; j++) s+=v_man_h[i*n+j];
+  int s2=s;
+
+
+  cudaMemset(v_man_h, 0,sizeof(int)*test*n);
+  cudaMemset(v_man, 0,sizeof(int)*test*n);
+  timer.Reset();
+  //memcp2<<<100,100,0, stream_comp>>>(v_man, v_man_d, sizeof(int)*n, test, stream_comp2);
+  memcp<<<100,1,0, stream_comp>>>(v_man, v_man_d, sizeof(int)*n, test, stream_comp2);
+  cudaStreamSynchronize(stream_comp);
+  cudaStreamSynchronize(stream_comp2);
+  double t3_1=timer.Elapsed();
+  cudaMemcpyAsync(v_man_h,v_man,sizeof(int)*n*test,cudaMemcpyDeviceToHost, stream_comp);
+  cudaStreamSynchronize(stream_comp);
+  cudaCheckError();
+  double t3_2=timer.Elapsed();
+
+  s=0;
+  for (int i=0; i<test; i++) for (int j=0; j<n; j++) s+=v_man_h[i*n+j];
+  int s3=s;
+
+  cudaMemset(v_man_h, 0,sizeof(int)*test*n);
+  cudaMemset(v_man, 0,sizeof(int)*test*n);
+  s=0;
+  for (int i=0; i<test; i++) for (int j=0; j<n; j++) s+=v_man_h[i*n+j];
+  int s1_1=s;
+  timer.Reset();
+  memcp2<<<100,100,0, stream_comp>>>(v_man, v_man_d, sizeof(int)*n, test, stream_comp2);
+  cudaStreamSynchronize(stream_comp);
+  cudaStreamSynchronize(stream_comp2);
+  double t4_1=timer.Elapsed();
+  cudaMemcpyAsync(v_man_h,v_man,sizeof(int)*n*test,cudaMemcpyDeviceToHost, stream_comp);
+  cudaStreamSynchronize(stream_comp);
+  cudaCheckError();
+  double t4_2=timer.Elapsed();
+
+  s=0;
+  for (int i=0; i<test; i++) for (int j=0; j<n; j++) s+=v_man_h[i*n+j];
+  int s4=s;
+
+
+  std::cout<<t1<<" "<<s1<<" "<<s1_1<<" "<<t2_1<< " "<<t2_2<<" "<<s2<<" "<<t3_1<<" "<<t3_2<<" "<<s3<<" "<<t4_1<<" "<<t4_2<<" "<<s4<<std::endl;
   
   return 0;
 #if 0
