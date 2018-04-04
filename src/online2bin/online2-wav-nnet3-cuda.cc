adcb9fa713431ff1e173e1193f00d2b94a4e7c48
EC2 Default User <ec2-user@ip-172-31-20-231.us-east-2.compute.internal>
2018-04-04 18:32:10 +0000

    each thread do instantiation

--- src/online2bin/online2-wav-nnet3-cuda.cc
+++ src/online2bin/online2-wav-nnet3-cuda.cc
@@ -143,16 +143,16 @@ int main(int argc, char *argv[]) {
 
 
     cuInit(0);
-#if HAVE_CUDA==1
-      CuDevice::Instantiate().SelectGpuId("yes");
-      CuDevice::Instantiate().AllowMultithreading();
-#endif
 
     CudaFst cuda_fst;
 
 #pragma omp parallel shared(po, cuda_fst) 
     {
       printf("Thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+#if HAVE_CUDA==1
+      CuDevice::Instantiate().SelectGpuId("yes");
+      CuDevice::Instantiate().AllowMultithreading();
+#endif
 
       // feature_opts includes configuration for the iVector adaptation,
       // as well as the basic features.
