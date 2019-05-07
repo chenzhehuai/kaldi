46ac424247b33f35a124f9e3591421a3ed15d8a4
Zhehuai Chen <chenzhehuai.sjtu@aispeech.com>
2019-05-07 21:03:31 +0800

    test 1stage fini; exp_dec/mmap.dec.1d2/ exp_dec/mmap.dec.2d/; not exp_dec/mmap.dec.1d3/

--- src/bin/latgen-faster-mapped.cc
+++ src/bin/latgen-faster-mapped.cc
@@ -34,28 +34,54 @@
 #include "../fstext/ThreadPool.h"
 #include "time.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
 using namespace fst;
+
+int is_mincore_page_resident(char p) {
+  return p & 0x1;
+}
+int64_t bytes2pages(int64_t bytes, int64 pagesize) {
+  return (bytes+pagesize-1) / pagesize;
+}
+float page_stat(string fst_in_str) {
+  auto fd = open(fst_in_str.c_str(), O_RDONLY, 0);
+  struct stat sb;
+  auto res = fstat(fd, &sb);
+  KALDI_ASSERT(!res);
+  int64 len_of_file = sb.st_size;
+  auto len_of_range = len_of_file;
+  void* mem = mmap(NULL, len_of_range, PROT_READ, MAP_SHARED, fd, 0);
+  int64 total_pages_in_core=0;
+  auto pagesize = sysconf(_SC_PAGESIZE);
+  auto pages_in_range = bytes2pages(len_of_range, pagesize);
+  int64_t total_pages=pages_in_range;
+  unsigned char*mincore_array = (unsigned char*)malloc(pages_in_range);
+  if (mincore(mem, len_of_range, mincore_array)) KALDI_ERR << "";
+  for (int i=0; i<pages_in_range; i++) {
+    if (is_mincore_page_resident(mincore_array[i])) {
+      total_pages_in_core++;
+    }
+  }
+  free(mincore_array);
+  if (munmap(mem, len_of_range)) KALDI_ERR<< "unable to munmap file";
+  return 1.0f*total_pages_in_core/total_pages;
+}
+
 
 template <typename Arc>
 size_t LoadFstState(const Fst<Arc>& fst, size_t s, bool change_seq = false) {
   size_t narcs = 0;
+  typename Arc::Weight weight;
   for (ArcIterator<Fst<Arc>> aiter(fst, s); !aiter.Done(); aiter.Next()) {
-    const auto &arc = aiter.Value();
-    Arc arc_new;
-    arc_new.nextstate = arc.nextstate;
-    arc_new.weight = arc.weight;
-    arc_new.ilabel = arc.ilabel;
-    arc_new.olabel = arc.olabel;
+    auto arc = aiter.Value();
+    weight=Times(weight, arc.weight); // in case aiter.Value() is optimized out
     narcs++;
     if (change_seq) {
-      for (ArcIterator<Fst<Arc>> aiter(fst, arc_new.nextstate); !aiter.Done(); aiter.Next()) {
-        const auto &arc = aiter.Value();
-        Arc arc_new;
-        arc_new.nextstate = arc.nextstate;
-        arc_new.weight = arc.weight;
-        arc_new.ilabel = arc.ilabel;
-        arc_new.olabel = arc.olabel;
-      }
+      KALDI_ASSERT(0);
     }
   }
   return narcs;
@@ -70,22 +96,132 @@ void PreloadFst2(ConstFst<StdArc> *decode_fst, size_t start, size_t end) {
   }
   KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<narcs<<" "<<time(NULL);
 }
-void PreloadFst(string fst_in_str, size_t start, size_t end) {
-  auto *decode_fst = dynamic_cast<ConstFst<StdArc>*>(fst::ReadFstKaldiGeneric(fst_in_str, true, "map", MAP_SHARED|MAP_POPULATE));
-  return;
+void PreloadFstC0(string fst_in_str, size_t start, size_t end) {
+  KALDI_LOG<<"loading_start ( "<<start<<" , "<<end<<" ) "<<time(NULL);
+  auto *decode_fst = dynamic_cast<ConstFst<StdArc>*>(fst::ReadFstKaldiGeneric(fst_in_str, true, "map", MAP_SHARED));
   KALDI_ASSERT(start>=0);
   KALDI_ASSERT(end <= decode_fst->NumStates());
-  KALDI_LOG<<"loading_start ( "<<start<<" , "<<end<<" ) "<<time(NULL);
   size_t narcs = 0;
   for (size_t i = start; i < end; i++) {
     narcs += LoadFstState(*decode_fst, i);
   }
+  delete decode_fst;
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
   KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<narcs<<" "<<time(NULL);
 }
-void ParaPreloadFst(string fst_in_str, size_t num_states, ThreadPool& pool, int nthreads) {
-  int num_states_per_thread = num_states/nthreads+1;
+void PreloadFstC_1(string fst_in_str, size_t start, size_t end) {
+  KALDI_LOG<<"loading_start ( "<<start<<" , "<<end<<" ) "<<time(NULL);
+  auto *decode_fst = dynamic_cast<ConstFst<StdArc>*>(fst::ReadFstKaldiGeneric(fst_in_str, true, "map", MAP_SHARED));
+  KALDI_ASSERT(start>=0);
+  KALDI_ASSERT(end <= decode_fst->NumStates());
+  size_t narcs = 0;
+  for (int64 i = end-1; i >= (int64)start; i--) {
+    narcs += LoadFstState(*decode_fst, i);
+  }
+  delete decode_fst;
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<narcs<<" "<<time(NULL);
+}
+
+
+void PreloadFstC1(string fst_in_str, size_t start, size_t end) {
+  KALDI_LOG<<"loading_start ( "<<start<<" , "<<end<<" ) "<<time(NULL);
+  auto *decode_fst = dynamic_cast<ConstFst<StdArc>*>(fst::ReadFstKaldiGeneric(fst_in_str, true, "map", MAP_SHARED|MAP_POPULATE));
+  KALDI_ASSERT(decode_fst);
+  delete decode_fst;
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<" "<<time(NULL);
+}
+void PreloadFstC2(string fst_in_str, size_t start, size_t end) {
+  KALDI_LOG<<"loading_start ( "<<start<<" , "<<end<<" ) "<<time(NULL);
+  auto pagesize = sysconf(_SC_PAGESIZE);
+  auto fd = open(fst_in_str.c_str(), O_RDONLY, 0);
+
+  struct stat sb;
+  auto res = fstat(fd, &sb);
+  KALDI_ASSERT(!res);
+  int64 len_of_file = sb.st_size;
+  auto len_of_range = len_of_file;
+  auto pages_in_range = (len_of_range+pagesize-1) / pagesize;
+
+  /*
+  if (posix_fadvise(fd, 0, len_of_range, POSIX_FADV_SEQUENTIAL)) 
+    KALDI_ERR << "fail in posix_fadvise";
+    */
+
+  void* mem = mmap(NULL, len_of_range, PROT_READ, MAP_SHARED, fd, 0);
+  uint junk_counter=0;
+  for (int i=0; i<pages_in_range; i++) {
+    junk_counter += ((char*)mem)[i*pagesize];
+  }
+  if (munmap(mem, len_of_range)) KALDI_ERR<< "unable to munmap file";
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<junk_counter<<" "<<time(NULL);
+}
+void PreloadFstC3(string fst_in_str, size_t start, size_t end) {
+  auto fd = open(fst_in_str.c_str(), O_RDONLY, 0);
+  struct stat sb;
+  auto res = fstat(fd, &sb);
+  KALDI_ASSERT(!res);
+  int64 len_of_file = sb.st_size;
+  auto len_of_range = len_of_file;
+  if (posix_fadvise(fd, 0, len_of_range, POSIX_FADV_WILLNEED)) 
+    KALDI_ERR << "fail in posix_fadvise";
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<len_of_range<<" "<<time(NULL);
+}
+void PreloadFstC4(string fst_in_str, size_t start, size_t end) {
+  auto fd = open(fst_in_str.c_str(), O_RDONLY, 0);
+  struct stat sb;
+  auto res = fstat(fd, &sb);
+  KALDI_ASSERT(!res);
+  int64 len_of_file = sb.st_size;
+  auto len_of_range = len_of_file;
+  auto pagesize = sysconf(_SC_PAGESIZE);
+  for (int64 i=len_of_range-1-pagesize; i >=0; i-=pagesize) {
+    if (posix_fadvise(fd, i, i+pagesize+1, POSIX_FADV_WILLNEED)) 
+      KALDI_ERR << "fail in posix_fadvise";
+  }
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<len_of_range<<" "<<time(NULL);
+}
+void PreloadFstC5(string fst_in_str, size_t start, size_t end) {
+  auto fd = open(fst_in_str.c_str(), O_RDONLY, 0);
+  struct stat sb;
+  auto res = fstat(fd, &sb);
+  KALDI_ASSERT(!res);
+  int64 len_of_file = sb.st_size;
+  auto len_of_range = len_of_file;
+  auto pagesize = sysconf(_SC_PAGESIZE);
+  for (int64 i=0; i < len_of_range; i+=pagesize) {
+    if (posix_fadvise(fd, i, i+pagesize, POSIX_FADV_WILLNEED)) 
+      KALDI_ERR << "fail in posix_fadvise";
+  }
+  KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
+  KALDI_LOG<<"loading_end ( "<<start<<" , "<<end<<" ) "<<len_of_range<<" "<<time(NULL);
+}
+
+
+
+
+void ParaPreloadFst(string fst_in_str, size_t num_states, ThreadPool& pool, int nthreads, int type, float ratio) {
+  int num_states_per_thread = (num_states*ratio)/nthreads+1;
   for(int i = 0; i < nthreads; ++i) { // TODO
-    pool.enqueue(&PreloadFst, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    if (type==0)
+      pool.enqueue(&PreloadFstC0, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type == -1)
+      pool.enqueue(&PreloadFstC_1, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type==1)
+      pool.enqueue(&PreloadFstC1, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type==2)
+      pool.enqueue(&PreloadFstC2, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type==3)
+      pool.enqueue(&PreloadFstC3, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type==4)
+      pool.enqueue(&PreloadFstC4, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+    else if (type==5)
+      pool.enqueue(&PreloadFstC5, fst_in_str, i*num_states_per_thread, std::min(num_states, (size_t)(i+1)*num_states_per_thread));
+
   }
   return;
 }
@@ -110,13 +246,17 @@ int main(int argc, char *argv[]) {
 
     std::string word_syms_filename;
     int mmap_mode=0;
+    int load_mode=0;
     int nthreads=0;
+    float load_ratio=1;
     config.Register(&po);
     po.Register("acoustic-scale", &acoustic_scale, "Scaling factor for acoustic likelihoods");
 
     po.Register("word-symbol-table", &word_syms_filename, "Symbol table for words [for debug output]");
     po.Register("allow-partial", &allow_partial, "If true, produce output even if end state was not reached.");
     po.Register("mmap-mode", &mmap_mode, "0 read 1 mmap(MAP_SHARED) 2 mmap(MAP_SHARED|MAP_POPULATE) ");
+    po.Register("load-mode", &load_mode, " ");
+    po.Register("load-ratio", &load_ratio, " ");
     po.Register("nthreads", &nthreads, "num threads to load fst");
 
     po.Read(argc, argv);
@@ -172,11 +312,11 @@ int main(int argc, char *argv[]) {
       }
       FLAGS_v=1;
       auto *decode_fst = dynamic_cast<ConstFst<StdArc>*>(fst::ReadFstKaldiGeneric(fst_in_str, true, map, mmap_flags));
+      KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
 
       ThreadPool pool(nthreads); // try nthreads/2 later
       if (nthreads) {
-        ParaPreloadFst(fst_in_str, decode_fst->NumStates(), pool, nthreads); // TODO: no improvement
-        //pool.enqueue(&PreloadFst2, decode_fst, 0, decode_fst->NumStates());
+        ParaPreloadFst(fst_in_str, decode_fst->NumStates(), pool, nthreads, load_mode, load_ratio); // TODO: no improvement
       }
 
       timer.Reset();
@@ -247,6 +387,7 @@ int main(int argc, char *argv[]) {
               << num_fail;
     KALDI_LOG << "Overall log-likelihood per frame is " << (tot_like/frame_count) << " over "
               << frame_count<<" frames. "<<time(NULL);
+    KALDI_LOG << "page_stat: " << page_stat(fst_in_str);
 
     delete word_syms;
     if (num_success != 0) return 0;
